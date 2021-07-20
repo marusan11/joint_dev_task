51aed9b1f6d599da2d7fba7f6f43c49039a8f41f
tomaru <tk.1126.hyt@gmail.com>
2021-07-20 23:58:56 +0900

    PHP基礎課題Q10-Q14

--- task.php
+++ task.php
@@ -88,7 +88,7 @@ $numbers1 = [1, 2, 3, 4, 5];
 
   # 以下に回答を記載
   function multiple_number($numbers1){
-    return $numbers1*10;
+    return $numbers1 * 10;
   }
   $numbers2 = array_map('multiple_number' ,$numbers1);
   print_r($numbers2); 
@@ -102,7 +102,7 @@ $array = ["1", "2", "3", "4", "5"];
   # 以下に回答を記載
   $array = array_map('intval',$array);
   //指定した変数を整数型に変換する関数 intval()
-  
+
   # 以下は変更しないで下さい
 var_dump($array);
 
@@ -147,42 +147,106 @@ $names = ["田中", "佐藤", "佐々木", "高橋"];
   } 
   print_r($names2);
 echo PHP_EOL;
+?>
 
+<?php
 print("#####q10#####".PHP_EOL);
 $foods = ["いか", "たこ", "うに", "しゃけ", "うにぎり", "うに軍艦", "うに丼", "高級なうに"];
 
   # 以下に回答を記載
+  $sarch = "うに";
+  $key = in_array($sarch,$foods);
+  //in_array関数:配列の中に特定の値が含まれているかどうか調べる時に使用。ある場合はture,ない場合はfalse。
 
+  if($key){
+    print_r("好物です");
+  }else{
+    print_r("まあまあ好です");
+  }
 echo PHP_EOL;
+?>
 
+<?php
 print("#####q11#####".PHP_EOL);
 $sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]];
 
   # 以下に回答を記載
+  //多次元配列から新しい配列を作成
+  $sports2 = [];
+  foreach($sports as $key => $sport) {
+    if(is_array($sport)) {
+      $sports2 = array_merge($sports2, $sport);
+    }else{
+      array_push($sports2, $sport);
+    }
+    //is_array:指定した変数が配列かどうかを調べる。
+    //配列ならarray_mergeで結合される（一つの配列になる）
+    //array_pushで要素を配列の最後に追加する。
+  }
 
+  // print_r($sports2);//一次元配列になったか確認//OK
+
+  $sports2 = array_unique($sports2);
+  //配列で重複しているものを削除
+  $sports2 = array_values($sports2);
+  //キーを振り直す
+
+  $sports3 = [];
+  foreach($sports2 as $key => $sport) {
+    $number = $key + 1;
+    $sport3 = "No".$number." ".$sport;
+    array_push($sports3, $sport3);
+  }
+  print_r("ユーザーの趣味一覧".PHP_EOL);
+  foreach($sports3 as $sport){
+    print($sport.PHP_EOL);
+  }
+  //foreachで配列の要素だけ順にを取り出す。
 echo PHP_EOL;
+?>
 
+<?php
 print("#####q12#####".PHP_EOL);
 $data = ["user" => ["name" => "satou", "age" => 33]];
 
   # 以下に回答を記載
-
+  $user = $data["user"];
+  print_r($user["name"]);
 echo PHP_EOL;
+?>
 
+<?php
 print("#####q13#####".PHP_EOL);
 $user_data = ["name" => "神里", "age" => 31, "address" => "埼玉"];
 $update_data = ["age" => 32, "address" => "沖縄"];
 
   # 以下に回答を記載
-
+  $result = array_merge($user_data, $update_data);
+  //array_merge:配列の結合の他に、keyが同じインデックスの場合は追加した配列に上書きされる。
+  print_r($result);
 echo PHP_EOL;
+?>
 
+<?php
 print("#####q14#####".PHP_EOL);
 $data = ["name" => "satou", "age" => 33, "address" => "saitama", "hobby" => "soccer", "email" => "hoge@fuga.com"];
 
-  # 以下に回答を記載
+  # 以下に回答を記載 
+  //新しい配列を作る。
+  $new_data = [];
+  //全てのキーを取り出す。
+  foreach($data as $key => $one_data) {
+    array_push($new_data, $one_data);
+    //array_push:配列の最後に要素を追加する
+  }
+  print_r($new_data);
+  //keyがなくなり、展示配列として出力される。
 
+
+  # 以下は変更しないで下さい
+var_dump($array);
 echo PHP_EOL;
+?>
 
 print("#####q15#####".PHP_EOL);
 $data1 = ["name" => "saitou", "hobby" => "soccer", "age" => 33, "role" => "admin"];
